a7d85d621cb573dc85c63f3806ac88d9bdf7a181
Rameez khan <sardarrameez5555@gmail.com>
2020-06-20 10:50:53 +0500

    Adding parent and child.js

---
+++ src/Child.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Child(){
+    return (
+        <div>
+            Hello from Child
+        </div>
+    )
+}
+export default Child;
---
+++ src/Parent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Child from './Child.js'
+function Parent(){
+    return (
+        <Child></Child>
+        
+    )
+}
+export default Parent;
